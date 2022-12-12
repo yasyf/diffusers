1e8cf48cc6f1dd3127e0a7796600f1ed733a9232
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-12 01:48:56 -0800

    support sharding

--- examples/dreambooth/train_dreambooth_flax.py
+++ examples/dreambooth/train_dreambooth_flax.py
@@ -682,7 +682,8 @@ def main():
                 deterministic=True,
                 capture_intermediates=True,
             )
-            jax.experimental.host_callback.id_print(y.latent_dist.__dict__)
+            print(y)
+            jax.experimental.host_callback.id_print(y[0].__dict__)
             return y["intermediates"]["quant_conv"]["__call__"]
             jax.debug.print("D: {d}", d=y.__dict__)
             xxx = jax.device_get(jnp.asarray([y.mean, y.logvar, y.std, y.var]))
