1ed1293cbbbc085d0c9100a75f44dc49a6184593
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-11 18:56:08 -0800

    debug

--- examples/dreambooth/train_dreambooth_flax.py
+++ examples/dreambooth/train_dreambooth_flax.py
@@ -682,7 +682,7 @@ def main():
 
     def cache_latents_sharded(batches):
         print("BATCHES", batches)
-        return jax.vmap(cache_latents)(batches)
+        return jax.vmap(cache_latents, axis_name="batch")(batches)
 
     # Cache latents
     if args.cache_latents:
