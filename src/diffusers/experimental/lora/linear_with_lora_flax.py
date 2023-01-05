97fb120245a384e4d117d27cd5f237c659c9eda6
Yasyf Mohamedali <yasyfm@gmail.com>
2023-01-05 14:31:50 -0800

    clone

--- src/diffusers/experimental/lora/linear_with_lora_flax.py
+++ src/diffusers/experimental/lora/linear_with_lora_flax.py
@@ -89,10 +89,6 @@ class FlaxLoraBase(nn.Module):
         targets: List[str],
         is_target: bool = False,
     ):
-        # model = model.bind({"params": params})
-        # if hasattr(model, "init_weights"):
-        #     model.init_weights(jax.random.PRNGKey(0))
-
         params = params.unfreeze() if isinstance(params, FrozenDict) else copy.copy(params)
         params_to_optimize = {}
 
@@ -154,6 +150,8 @@ def FlaxLora(model: Type[nn.Module], targets=["FlaxAttentionBlock"]):
         def from_pretrained(cls, *args, **kwargs):
             instance, params = cast(Type[FlaxModelMixin], model).from_pretrained(*args, **kwargs)
 
+            instance = instance.bind({"params": params})
+            instance.init_weights(jax.random.PRNGKey(0))
             params, mask = FlaxLoraBase.inject(params, instance, targets=targets)
 
             subattrs = {f.name: getattr(instance, f.name) for f in dataclasses.fields(instance) if f.init}
